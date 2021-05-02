e76131d38766ff8a76b001c49b11089a1155bbb8
Andrei Lysenkovskiy <andreilysen@ukr.net>
2021-05-02 13:37:41 +0300

    Готовая работа

--- js/create-gallery.js
+++ js/create-gallery.js
@@ -3,8 +3,9 @@ import gallerys from './gallery-items.js'
 const galleryRef = document.querySelector('.js-gallery');
 const lightboxRef = document.querySelector('.js-lightbox');
 const lightboxImage = document.querySelector('.lightbox__image');
-const lightboxContent = document.querySelector('.lightbox__content');
+// const lightboxContent = document.querySelector('.lightbox__content');
 const btnCloseModal = document.querySelector('[data-action="close-lightbox"]');
+const lightboxOverlay = document.querySelector('.lightbox__overlay');
 
 const createImage = ({ preview, original, description }) => {
     const galleryItem = document.createElement('li');
@@ -22,9 +23,37 @@ const createImage = ({ preview, original, description }) => {
     return galleryItem
 };
 
-const createGallery = gallerys.map(gallery => createImage(gallery));
+const createGallery = gallerys.map((gallery) => createImage(gallery));
 
 galleryRef.append(...createGallery);
 
+const escCloseModal = (e) => {
+    if (e.code === 'Escape') {
+      closeModal()  
+    }
+}
 
-// console.log(createGallery(d))
+const closeModal = () => {
+    lightboxRef.classList.remove('is-open');
+    btnCloseModal.removeEventListener('click', closeModal);
+    window.removeEventListener('keydown', escCloseModal);
+
+    lightboxImage.src = '';
+    lightboxImage.alt = '';
+}
+
+const openModal = (e) => {
+    e.preventDefault()
+    if (!e.target.classList.contains('gallery__image')) {
+        return
+    }
+    btnCloseModal.addEventListener('click', closeModal);
+    lightboxOverlay.addEventListener('click', closeModal);
+    window.addEventListener('keydown', escCloseModal);
+    lightboxRef.classList.add('is-open');    
+    lightboxImage.src = e.target.dataset.source;
+    lightboxImage.alt = e.target.alt;
+    console.log(e.target.dataset.source)
+}
+
+galleryRef.addEventListener('click', openModal)
